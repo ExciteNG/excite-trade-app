fbe2a796d856815c1bba5b60d91b146829502f6a
oladips772 <oladips200@gmail.com>
2025-09-28 10:01:50 +0100

    chore: update dependencies and add Tailwind CSS configuration
    - Downgraded expo version from 54.0.10 to 54.0.7
    - Added nativewind as a dependency
    - Added devDependencies for Babel, nativewind, and Tailwind CSS
    - Created babel.config.js to include nativewind plugin
    - Created metro.config.js to configure Metro bundler
    - Created postcss.config.js for Tailwind CSS and autoprefixer
    - Created tailwind.config.js to define Tailwind CSS content paths and theme

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500">
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
