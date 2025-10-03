ff39e710860765b077d97bbf41dc0e98888b8c45
oladips772 <oladips200@gmail.com>
2025-10-03 09:13:07 +0100

    Implement code changes to enhance functionality and improve performance

--- components/TopCommodities.jsx
+++ components/TopCommodities.jsx
@@ -3,6 +3,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+const data = [
+  {
+    name: "Groundnut",
+    image: require("../assets/groundnut.png"),
+  },
+];
+
 const TopCommodities = () => {
   return (
     <View>
