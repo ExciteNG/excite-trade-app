19633f8bcd876ada1e90305784d46e316c7875c2
Oluwayelu <oluwayeluifeoluwa@gmail.com>
2026-04-20 09:28:54 +0100

    fix: update API URL to new ngrok endpoint

--- url.js
+++ url.js
@@ -3,4 +3,4 @@
 // export const url = "https://excite-trade-api-production.up.railway.app/api/v1";
 // export const url =
 //   "https://excite-trade-grb9dyfndagef6ak.westus3-01.azurewebsites.net/api/v1";
-export const url = "https://3ef0-197-211-57-3.ngrok-free.app/api/v1";
+export const url = "https://a011-102-88-55-74.ngrok-free.app/api/v1";
